b4a2d09a963ff5637ec44105cc5a891c5f817049
Herlan Avila Peña <herlan.avila12@gmail.com>
2026-09-21 01:01:20 -0400

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const http = require('http');
 const { Server } = require('socket.io');
 const { Pool } = require('pg');
 const jwt = require('jsonwebtoken');
+const cors = require('cors');
 
 const app = express();
 const server = http.createServer(app);
