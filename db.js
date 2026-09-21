57d8785ea8d4505f797b3bb55d0235ff11db0b1f
Herlan Avila Peña <herlan.avila12@gmail.com>
2026-09-20 23:32:38 -0400

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'tu_usuario_postgres',
-    host: 'localhost',
-    database: 'smswait_db', // Nombre de tu base de datos
-    password: 'tu_contraseña',
-    port: 5432,
+    connectionString: process.env.DATABASE_URL || 'postgresql://neondb_owner:npg_RjZUaWqb0tz4@ep-small-field-b4db0i6v-pooler.c-6.us-east-2.aws.neon.tech/neondb?sslmode=require',
+    ssl: {
+        rejectUnauthorized: false // Requerido para conexiones seguras en la nube (Neon/Render)
+    }
 });
 
 module.exports = {
